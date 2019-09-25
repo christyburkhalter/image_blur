0f52bbcb8cdf1bdcafd4bcbe164ff56062dda304
christyburkhalter <53546957+christyburkhalter@users.noreply.github.com>
2019-09-24 20:59:33 -0500

    Add files via upload

--- image_blur/image_blur2.rb
+++ image_blur/image_blur2.rb
@@ -1,28 +1,38 @@
 class Image
+  attr_accessor :image
 
-    def initialize(pixel)
-      @pixel = pixel
+    def initialize(image)
+      @image = image
     end
 
-    def find_ones
+    def get_ones
       ones = []
-      pixel.each_with_index do |n, row|
-        num.each_with_index do |i, col|
-          if i == 1
-            ones << [row, col]
+      image.each_with_index do |num, row_index|
+        num.each_with_index do |item, column_index|
+          if item == 1
+            ones << [row_index, column_index]
           end
         end
       end
 
       def blur
-        number_of_rows = pixel.length
-        number_of_columns = pixel[0].length
-        find_ones.each do |coord|
-          pixel[coord[0]][coord[1] + 1] = 1 unless coord[1]+1 > number_of_columns
-          pixel[coord[0]][coord[1] - 1] = 1 unless coord[1] == 0
-          pixel[coord[0] + 1][coord[1]] = 1 unless coord[0] + 1 > number_of_rows
-          pixel[coord[0] - 1][coord[1]] = 1 unless corrd[0] == 0
-      end
+        number_of_rows = image.length
+        number_of_columns = image[0].length
+        get_ones.each do |coord|
+          image[coord[0]][coord[1] + 1] = 1 unless coord[1]+1 > number_of_columns
+          image[coord[0]][coord[1] - 1] = 1 unless coord[1] == 0
+          image[coord[0] + 1][coord[1]] = 1 unless coord[0] + 1 > number_of_rows
+          image[coord[0] - 1][coord[1]] = 1 unless corrd[0] == 0
+        end
     end
 end
+
+
+image = Image.new [
+  [0,0,1,0],
+  [0,1,0,0],
+  [0,0,0,1],
+  [0,1,0,0]
+]
+
 image.blur
